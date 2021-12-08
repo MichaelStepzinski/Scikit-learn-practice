54383ab147538791e1af4bb35adea16f3eca8bab
DesertPangolin <michaelpstepzinski@gmail.com>
2021-12-08 03:44:23 -0800

    fixed utilities

--- utilities.py
+++ utilities.py
@@ -4,7 +4,8 @@
 
 import numpy as np
 import sklearn as sk
-from sklearn.feature_extraction.text import CountVectorizer
+#from sklearn.datasets.load_files
+#from sklearn.feature_extraction.text import CountVectorizer
 import sys
 import os
 import random
@@ -16,76 +17,47 @@ def generate_vocab(dir, min_count, max_files):
     posfilenames = os.listdir(posdir)
     negfilenames = os.listdir(negdir)
     
-    # if max_fiels is -1, then get every file
+    # if max_files is -1, then get every file
     if max_files == -1:
         file_count = len(posfilenames) + len(negfilenames)
     else:
-        file_count = max_files
+        #file_count = max_files
+        posfilenames = posfilenames[:int(max_files/2)]
+        negfilenames = negfilenames[:int(max_files/2)]
 
-    # create corpus containing each string
-    corpus = []
+    # create corpus dictionary
+    corpus = {}
 
-    # for filename in pos, add data to corpus
-    for filename in posfilenames:
-        with open(posdir + filename, 'r') as data:
-            corpus.append(data.read())
-        if len(corpus) == int(file_count/2):
-            break
-    for filename in negfilenames:
-        with open(negdir + filename, 'r') as data:
-            corpus.append(data.read())
-        if len(corpus) == file_count:
-            break
-    # use sklearn to vectorize corpus
-    vectorizer = CountVectorizer()
-    X = vectorizer.fit_transform(corpus)
+    generate_corpus(posfilenames, posdir, corpus)
+    generate_corpus(negfilenames, negdir, corpus)
 
-    # get vectorized words from corpus
-    words = vectorizer.get_feature_names_out()
-    # get count of each vectorized word in corpus
-    counts = X.toarray()
-
-    # declare final vocab list
-    vocab = []
-
-    # if word occurs min_count times, add to vocabulary
-    sums = counts.sum(axis=0)
-    for word, count in zip(words, sums):
-        if count >= min_count:
-            vocab.append(word)
+    # using corpus, get all words that occur more than min_count
+    vocab = [k for k,v in corpus.items() if v > 1]
+    
     return vocab
 
+
 def create_word_vector(fname, vocab):
+    # exclude these characters for data processing
+    exclude = '!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~0123456789'
+    
+    # create word vector
+    word_vector = np.zeros((1, len(vocab)))
+
     # read one file
     with open(fname, 'r') as data:
-        # raw is in list format to work with vectorizer.fit_transform
-        raw = [data.read()]
-
-    # vectorize this file
-    vectorizer = CountVectorizer()
-    X = vectorizer.fit_transform(raw)
-    # get vectorized words from file
-    file_words = vectorizer.get_feature_names_out()
-    # get count of each vectorized word in file
-    counts = X.toarray()
-    # since raw was in list format, it gets interpreted as 2D array
-    # this is a workaround to get everything working
-    counts = counts[0]
-    # make counts a list to be input into a dictionary
-    counts = counts.tolist()
-
-    # occurences per word in file
-    file_word_dictionary = dict(zip(file_words, counts))
-
-    # occurences per word in vocab
-    word_dictionary = dict.fromkeys(vocab, '0')
-
-    # each time word in vocab occurs, increment spot in word_vector
-    for word in file_word_dictionary:
-        if word in vocab:
-            # increase occurence count
-            word_dictionary[word] = file_word_dictionary[word]
-    word_vector = np.fromiter(word_dictionary.values(), dtype=int)
+        # process data
+        temp = data.read()
+        temp = temp.lower()
+        for s in exclude:
+            temp = temp.replace(s, '')
+        temp = temp.split()
+        # if temp word is vocab word add to word vector
+        for word in temp:
+            if word in vocab:
+                # add word counts to word vector
+                index = vocab.index(word)
+                word_vector[0][index] += 1
 
     return word_vector
 
@@ -108,14 +80,37 @@ def load_data(dir, vocab, max_files):
 
     X_data = np.zeros(shape=(file_count, len(vocab)))
     Y_data = np.zeros(shape=(file_count))
+    # first half of Y is always positive reviews
+    # last half of Y is always negative reviews
+    # even when max_value -1 is used
     Y_data[:int(file_count/2)] = 1
     # for the first max_files filenames,
     #  generate vector and add to master dataset
     #  in dataset, positive example is 1, negative is 0
     for num, filename in zip(range(int(file_count/2)), posfilenames):
         X_data[num] = create_word_vector(posdir + filename, vocab)
+        print('pos ' + str(num))
     for num, filename in zip(range(int(file_count/2)), negfilenames):
         X_data[num] = create_word_vector(negdir + filename, vocab)
+        print('neg ' + str(num))
 
     return X_data, Y_data
             
+def generate_corpus(filenames, dir, corpus):
+    # punctuation exclusion list
+    exclude = '!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~0123456789'
+    for filename in filenames:
+        with open(dir + filename, 'r') as data:
+            # read data and process
+            temp = data.read()
+            temp = temp.lower()
+            for s in exclude:
+                temp = temp.replace(s, '')
+            # data is processed, add to corpus dictionary
+            # value:key = word:occurrences
+            temp = temp.split()
+            for word in temp:
+                if word in corpus:
+                    corpus[word] += 1
+                else:
+                    corpus[word] = 1

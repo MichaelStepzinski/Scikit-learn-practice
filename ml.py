88b7a414bb4d7c4c940eef7f71058bd1c0ebb3fd
DesertPangolin <michaelpstepzinski@gmail.com>
2021-12-08 03:44:43 -0800

    fixed ml

---
+++ ml.py
@@ -0,0 +1,52 @@
+# Author:   Michael Stepzinski
+# Date:     7 December, 2021
+# Purpose:  CS422 Project 5 Scikit-learn testing - ml
+
+from sklearn.metrics import f1_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import Perceptron
+from sklearn.neural_network import MLPClassifier
+from sklearn.utils._testing import ignore_warnings
+from sklearn.exceptions import ConvergenceWarning
+from sklearn import svm
+
+def dt_train(X,Y):
+    clf = DecisionTreeClassifier(random_state=0)
+    clf = clf.fit(X, Y)
+    return clf
+
+def kmeans_train(X):
+    return KMeans(n_clusters=2, random_state=0).fit(X)
+
+def knn_train(X,Y,K):
+    neigh = KNeighborsClassifier(n_neighbors=K)
+    return neigh.fit(X, Y)
+
+def perceptron_train(X,Y):
+    clf = Perceptron(random_state=0)
+    return clf.fit(X, Y)
+
+@ignore_warnings(category=ConvergenceWarning)
+def nn_train(X,Y,hls):
+    clf = MLPClassifier(hls, random_state=0, max_iter=300)
+    return clf.fit(X,Y)
+
+def pca_train(X,K):
+    pca = PCA(n_components=K)
+    return pca.fit(X)
+
+def pca_transform(X,pca):
+    return pca.transform(X)
+
+def svm_train(X,Y,k):
+    clf = svm.SVC(kernel=k)
+    return clf.fit(X, Y)
+
+def model_test(X,model):
+    return model.predict(X)
+
+def compute_F1(Y,Y_hat):
+    return f1_score(Y, Y_hat)
